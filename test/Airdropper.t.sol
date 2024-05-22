b6d3f950693bb9d1c09b0cd9acbbf9b040294146
antisaa <antesosa95@gmail.com>
2024-05-22 14:11:15 +0200

    Add integer parsing

--- test/Airdropper.t.sol
+++ test/Airdropper.t.sol
@@ -57,6 +57,8 @@ contract AirdropperTest is Test {
 
     function test_readCsv() public {
         string memory eof = "";
+        string memory line = vm.readLine("test.csv");
+        uint256 parsedSum = 0;
         while (true) {
             string memory line = vm.readLine("test.csv");
             if (keccak256(bytes(line)) == keccak256(bytes(eof))) {
@@ -69,9 +71,22 @@ contract AirdropperTest is Test {
             for (uint i = 0; i < parts.length; i++) {
                 parts[i] = s.split(delim).toString();
             }
-            for (uint i = 0; i < parts.length; i++) {
-                console.logString(parts[i]);
+            uint256 parsedInt = stringToUint(parts[1]);
+            console.logUint(parsedInt);
+            parsedSum += parsedInt;
+        }
+        console.logUint(parsedSum);
+    }
+
+    function stringToUint(string memory s) public pure returns (uint) {
+        bytes memory b = bytes(s);
+        uint result = 0;
+        for (uint256 i = 0; i < b.length; i++) {
+            uint256 c = uint256(uint8(b[i]));
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
             }
         }
+        return result;
     }
 }
